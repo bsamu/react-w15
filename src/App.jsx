8b56a7d0ce3b1208431348a3adce52fe3cf3589b
bsamu <bsamuel0220@gmail.com>
2022-02-17 11:00:25 +0100

    login added

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ function App() {
 
   const [authUser, setAuthUser] = useState('');
   const [authPassword, setAuthPassword] = useState('');
-
   const [todo, setTodo] = useState('');
+
+  const [sectionToAppear, setSectionToAppear] = useState('registration')
 
   const signUp = async () => {
     try {
@@ -18,6 +19,7 @@ function App() {
         password: passwordValue
       })
       alert("Succesful signup")
+      setSectionToAppear('login')
       setNameValue('')
       setPasswordValue('')
     } catch (err) {
@@ -50,21 +52,55 @@ function App() {
     }
   }
 
+  const login = async () => {
+    try {
+      await http.post('http://localhost:4000/api/login', {
+
+    }, {
+        headers: {
+          authorization: authUser+':::'+authPassword
+        }
+      })
+      console.log(authUser, authPassword);
+      setSectionToAppear('todos')
+
+    } catch (err) {
+      alert('Wrong username or password')
+    }
+  }
+
+  const signOut = () => {
+    setAuthUser('')
+    setAuthPassword('')
+    setSectionToAppear('login')
+  }
+
   return (
     <main>
-      <section className="App">
+
+      {sectionToAppear === 'registration' && <section>
         <h1>Registration</h1>
         <input type='text' placeholder='username' value={nameValue} onChange={(e) => setNameValue(e.target.value)} />
         <input type='password' placeholder='password' value={passwordValue} onChange={(e) => setPasswordValue(e.target.value)} />
         <button onClick={signUp}>Sign up</button>
-      </section>
-      <section>
+        <button onClick={() => setSectionToAppear('login')}>I already have an account</button>
+      </section>}    
+
+      {sectionToAppear === 'login' && <section>
+        <h1>Login</h1>
+        <input type="text" placeholder="authUsername" value={authUser} onChange={(e) => setAuthUser(e.target.value)}/>
+        <input type="password" placeholder="authPassword" value={authPassword} onChange={(e) => setAuthPassword(e.target.value)}/>
+        <button onClick={() => setSectionToAppear('registration')}>I don't have an account</button>
+        <button onClick={login}>Log in</button>
+      </section>}
+
+      {sectionToAppear === 'todos' && <section>
         <h1>Todos</h1>
-        <input type="text" placeholder="username" value={authUser} onChange={(e) => setAuthUser(e.target.value)}/>
-        <input type="password" placeholder="password" value={authPassword} onChange={(e) => setAuthPassword(e.target.value)}/>
         <input type='text' placeholder='todo' value={todo} onChange={(e) => setTodo(e.target.value)} />
         <button onClick={addTodo} disabled={!todo}>Add todo</button>
-      </section>
+        <button onClick={signOut}>Sign out</button>
+      </section>}
+      
     </main>
   );
 }
